2a51937a9c241a6b648b99406e44cd0c4247c743
Alexandru Smochina <146827365+AlexSmochina@users.noreply.github.com>
2024-10-30 16:29:34 +0100

    commit

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+def on_button_pressed_a():
+    global forma
+    forma += 1
+    if forma == 1:
+        basic.show_icon(IconNames.SMALL_SQUARE)
+    elif forma == 2:
+        basic.show_icon(IconNames.SQUARE)
+    elif forma == 3:
+        basic.show_icon(IconNames.SCISSORS)
+    else:
+        forma = 0
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 def on_gesture_shake():
     global parar
-    parar = randint(0, 3)
+    parar = randint(1, 3)
     if parar == 1:
         basic.show_icon(IconNames.SMALL_SQUARE)
     elif parar == 2:
@@ -9,10 +22,29 @@ def on_gesture_shake():
         basic.show_icon(IconNames.SCISSORS)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
+def on_button_pressed_b():
+    global forma_bot
+    forma_bot = randint(1, 3)
+    if forma_bot == 1 and forma == 1:
+        basic.show_string("EMPATE")
+    elif forma_bot == 2 and forma == 2:
+        basic.show_string("EMPATE")
+    elif forma_bot == 3 and forma == 3:
+        basic.show_string("EMPATE")
+    elif forma_bot == 1 and forma == 3:
+        basic.show_string("HAS PERDIDIO")
+    elif forma_bot == 2 and forma == 1:
+        basic.show_string("HAS PERDIDIO")
+    elif forma_bot == 3 and forma == 2:
+        basic.show_string("HAS PERDIDIO")
+    else:
+        basic.show_string("HAS GANADO")
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+forma_bot = 0
 parar = 0
+forma = 0
+basic.show_string("JUEGO")
 basic.show_icon(IconNames.SMALL_SQUARE)
-basic.pause(1000)
 basic.show_icon(IconNames.SQUARE)
-basic.pause(1000)
 basic.show_icon(IconNames.SCISSORS)
-basic.pause(1000)
--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    forma += 1
+    if (forma == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (forma == 2) {
+        basic.showIcon(IconNames.Square)
+    } else if (forma == 3) {
+        basic.showIcon(IconNames.Scissors)
+    } else {
+        forma = 0
+    }
+})
 input.onGesture(Gesture.Shake, function () {
-    parar = randint(0, 3)
+    parar = randint(1, 3)
     if (parar == 1) {
         basic.showIcon(IconNames.SmallSquare)
     } else if (parar == 2) {
@@ -8,10 +20,28 @@ input.onGesture(Gesture.Shake, function () {
         basic.showIcon(IconNames.Scissors)
     }
 })
+input.onButtonPressed(Button.B, function () {
+    forma_bot = randint(1, 3)
+    if (forma_bot == 1 && forma == 1) {
+        basic.showString("EMPATE")
+    } else if (forma_bot == 2 && forma == 2) {
+        basic.showString("EMPATE")
+    } else if (forma_bot == 3 && forma == 3) {
+        basic.showString("EMPATE")
+    } else if (forma_bot == 1 && forma == 3) {
+        basic.showString("HAS PERDIDIO")
+    } else if (forma_bot == 2 && forma == 1) {
+        basic.showString("HAS PERDIDIO")
+    } else if (forma_bot == 3 && forma == 2) {
+        basic.showString("HAS PERDIDIO")
+    } else {
+        basic.showString("HAS GANADO")
+    }
+})
+let forma_bot = 0
 let parar = 0
+let forma = 0
+basic.showString("JUEGO")
 basic.showIcon(IconNames.SmallSquare)
-basic.pause(1000)
 basic.showIcon(IconNames.Square)
-basic.pause(1000)
 basic.showIcon(IconNames.Scissors)
-basic.pause(1000)
